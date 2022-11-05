e72fc83ea65274d6ea8978a6bdd493c75f7b6104
pola1991 <pa.albornoz@duocuc.cl>
2022-11-04 22:50:20 -0300

    paginas

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Empleado)
+admin.site.register(Servicio)
+admin.site.register(Cliente)
+admin.site.register(Cuenta)
+admin.site.register(Reserva)
